785818c034a34734fc051e0cd2d0e40a32ebb759
Henrique Dezani <dezani@gmail.com>
2021-06-07 19:02:50 -0300

    Código inicial

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-# socket_ = SocketIO(app)
-
 
 @app.route('/')
 def hello():
@@ -9,12 +7,3 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
-
-# https://realpython.com/flask-by-example-part-1-project-setup/
-# $ touch app.py .gitignore README.md requirements.txt
-# $ python -m pip install Flask==1.1.1
-# $ python -m pip freeze > requirements.txt
-# $ python app.py
-
-# https://medium.com/swlh/implement-a-websocket-using-flask-and-socket-io-python-76afa5bbeae1
-
